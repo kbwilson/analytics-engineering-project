5199031279c6bd3e8f9438b94ec5eddf0b455300
kbwilson <kwilson818@gmail.com>
2025-08-08 18:09:23 -0400

    Update int_customers.sql

--- dbt/models/intermediate/int_customers.sql
+++ dbt/models/intermediate/int_customers.sql
@@ -1,6 +1,3 @@
--- stg_customers has a column named `name`, not `customer_name`.
--- Alias it to `customer_name` for clarity downstream.
-
 WITH orders AS (
   SELECT *
   FROM {{ ref('stg_orders') }}
@@ -20,4 +17,4 @@ LEFT JOIN orders o
   ON o.customer_id = c.customer_id
 GROUP BY
   c.customer_id,
-  c.name;
+  c.name
