142778d6f8b0a9ca9affbf0ff8d0314bd0bd07be
kbwilson <kwilson818@gmail.com>
2025-08-08 18:08:53 -0400

    Update int_time_aggregates.sql

--- dbt/models/intermediate/int_time_aggregates.sql
+++ dbt/models/intermediate/int_time_aggregates.sql
@@ -1,14 +1,11 @@
--- BigQuery: DATE_TRUNC takes (date_expr, date_part) with an unquoted date part
--- Also avoid aliasing to the bare word "day" just in case; use day_date.
-
 WITH orders AS (
   SELECT *
   FROM {{ ref('stg_orders') }}
 )
 SELECT
-  DATE_TRUNC(order_date, DAY) AS day_date,
+  order_date AS day_date,
   SUM(order_amount) AS daily_sales,
   COUNT(order_id) AS daily_orders
 FROM orders
 GROUP BY day_date
-ORDER BY day_date;
+ORDER BY day_date
